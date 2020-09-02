c46b2049e54bcf4e23a9265d7334b2a67daa8216
Manumathew01 <manu.thoppil01@gmail.com>
2020-09-02 15:35:03 +0530

    Updates

---
+++ precache-manifest.2fd4e09c823286df91e17ec5e66d8364.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cfb2d47187e17531294b8330a1f058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "614d6fad7294ed6000c5",
+    "url": "/static/css/main.d2651ca9.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614d6fad7294ed6000c5",
+    "url": "/static/js/main.ace2378f.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
